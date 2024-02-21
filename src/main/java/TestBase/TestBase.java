316a52fa3910a0ffa0a69c6353b244c79eba10f6
Suraj naik <suraj.naik@teknotrait.com>
2024-02-21 17:00:18 +0530

    set up the groundwork for the Luma project with POM

---
+++ src/main/java/TestBase/TestBase.java
@@ -0,0 +1,25 @@
+package TestBase;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestBase {
+	
+	public static WebDriver driver;
+	
+	public void browserLaunch()
+	{
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		driver.get("https://magento.softwaretestingboard.com/");
+	}
+	
+	public void quitBrowser()
+	{
+		driver.quit();
+	}
+
+}
---
+++ src/test/java/Runner/Runner.java
@@ -0,0 +1,23 @@
+package Runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/Feature", 
+                 glue = { "StepDefinition","Hooks" },
+                 //tags = "@AddPayGrades", 
+                 monochrome = true, 
+                 dryRun = false,
+                 plugin = {
+                	                "pretty",
+                	                "html:target/cucumber-reports/cucumber.html",
+                	                "json:target/cucumber-reports/cucumber.json",
+                	                "junit:target/cucumber-reports/Cucumber.xml"
+                	      })
+public class Runner {
+	
+
+}
